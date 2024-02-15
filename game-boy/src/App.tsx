da0f9367215e6f22b5e8734dc51006cd689364e4
Satish Patil <satsvelke@gmail.com>
2024-02-15 20:43:14 +0530

    created response layout

--- game-boy/src/App.tsx
+++ game-boy/src/App.tsx
@@ -1,9 +1,26 @@
-import { Button } from "@chakra-ui/react";
+import { Grid, GridItem, Show } from "@chakra-ui/react";
 
 function App() {
   return (
     <>
-      <Button colorScheme="blue">Button</Button>
+      <Grid
+        templateAreas={{
+          base: `"nav" "main"`,
+          lg: `"nav nav" "aside main"`,
+        }}
+      >
+        <GridItem bg="coral" area="nav">
+          Nav
+        </GridItem>
+        <Show above="lg">
+          <GridItem bg="red" area="aside">
+            aside
+          </GridItem>
+        </Show>
+        <GridItem bg="blue" area="main">
+          main
+        </GridItem>
+      </Grid>
     </>
   );
 }
