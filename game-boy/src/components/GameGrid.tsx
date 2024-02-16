0d00e35ea9bf8f9bb05abce0baace3aad0b2b723
Satish Patil <satsvelke@gmail.com>
2024-02-17 01:29:14 +0530

    refactor game   grid

--- game-boy/src/components/GameGrid.tsx
+++ game-boy/src/components/GameGrid.tsx
@@ -15,9 +15,9 @@ interface GameGridProps {
 const GameGrid = ({ gameQuery }: GameGridProps) => {
   const { data, error, isLoading } = useGames(gameQuery);
 
+  if (error) return <Text>{error}</Text>;
   return (
     <>
-      {error && <Text>{error}</Text>}
       <SimpleGrid columns={{ sm: 1, md: 2, lg: 3, xl: 5 }} spacing={5}>
         {isLoading &&
           skelotons.map((sk) => (
